e193c436fc4160200c9b881c4bb16c4669b115e8
Pavel <samasyt@mail.ru>
2025-04-19 12:02:05 +0300

    feat: add

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,36 @@
+<script setup lang="ts">
+import type { Post } from "@/data/posts";
+import PostCard from "~/components/PostCard.vue";
+import { getPostsUrl } from "~/data/posts";
+
+const { data: posts } = await useFetch<Post[]>(getPostsUrl());
+</script>
+
 <template>
-  <h1>Hello World</h1>
+  <main>
+    <h1>Nuxt.js exapmles</h1>
+    <div>
+      <PostCard v-for="post in posts" :key="post.id" :post />
+    </div>
+  </main>
 </template>
+
+<style scoped lang="scss">
+@use "@/assets/styles/colors.scss";
+
+.card {
+  padding: 15px;
+  border: 1px solid colors.$green-dark;
+  border-radius: 5px;
+  margin-bottom: 25px;
+  font-size: 12pt;
+}
+
+.card .title {
+  font-size: 20pt;
+}
+
+.card .category {
+  margin: 5px 0px;
+}
+</style>
